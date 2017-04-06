3539bad3c7fd19031e1f38124bd8c8efdbd7e635
Jiawei Luo <weiwei100fun@hotmail.com>
2017-04-05 21:30:15 -0400

    Add website icon

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ require('./hbshepler/hbshelpers')(hbs);
 require('amui-hbs-helper')(hbs);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
