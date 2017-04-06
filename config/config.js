0bb634202d7e3582ad7b4e1912a6d38fbc1ea05c
Jiawei Luo <weiwei100fun@hotmail.com>
2017-04-05 20:53:47 -0400

    Prepare for deployment

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     siteName: 'zineNews',
-    database: 'mongodb://zn:111111@localhost:27017/zineNews',
+    database: 'mongodb://zn:222222@ds019633.mlab.com:19633/heroku_8b93hwqw',
     crawlerTimeout: 540000,
     requestTimeout: 10000,
     defaultPageSearch: 3,
     scheduleRule: [10, 20, 30, 40, 50, 60],
-    siteUrl: 'http://jiawei.io:3000'
+    siteUrl: 'https://zine-news.herokuapp.com'
 };
